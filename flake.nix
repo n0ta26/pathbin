792dacfb835453c9c600ad5771380ac2e1740940
n0ta <teroro1226@outlook.jp>
2026-05-11 02:37:08 +0900

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "pathbin - Inspect executable binaries from PATH";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustc
+            cargo
+
+            rustfmt
+
+            clippy
+
+            rust-analyzer
+
+            git
+          ];
+
+          shellHook = ''
+            echo "pathbin Rust development environment"
+            echo "Rust: $(rustc --version)"
+            echo "Cargo: $(cargo --version)"
+          '';
+        };
+      }
+    );
+}
